002f20d09f406c723fde50414715f890004e52c7
Adnan Hussain <7xy.plane@gmail.com>
2024-01-05 19:26:21 +0530

    updated test

--- tests/Feature/MarkdownTest.php
+++ tests/Feature/MarkdownTest.php
@@ -19,48 +19,6 @@ class MarkdownTest extends TestCase
         dd($parsedown->text('#Heading'));
     }
 
-    protected function getPackageProviders($app)
-    {
-        return [
-            ParsedownServiceProvider::class,
-        ];
-    }
-    public function testPackageProvidersAreRegistered()
-    {
-        $app = $this->createApplication();
-
-        $providers = $this->getPackageProviders($app);
-
-        // Your test logic to assert the package providers registration
-        $this->assertTrue(in_array(ParsedownServiceProvider::class, $providers));
-    }
-
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = new \Illuminate\Foundation\Application();
-    
-        return $app;
-    }
-
-    // /**
-    //  * Get package providers.
-    //  *
-    //  * @param  \Illuminate\Foundation\Application  $app
-    //  * @return array
-    //  */
-    // protected function getPackageProviders($app)
-    // {
-    //     return [
-    //         ParsedownServiceProvider::class,
-    //         // Add other providers if needed
-    //     ];
-    // }
-
     public function testHelperWorks()
     {
         $result = parsedown('# Heading 1');
@@ -82,21 +40,10 @@ class MarkdownTest extends TestCase
         $this->assertEquals('<h1>Heading 1</h1>', $result);
     }
 
-    public function serviceTest()
+    protected function getPackageProviders($app)
     {
-        // Reset the application to its initial state
-        $this->app = $this->createApplication();
-
-        // Register the service provider
-        $this->app->register(ParsedownServiceProvider::class);
-
-        // Assert that the Parsedown instance is bound in the container
-        $this->assertInstanceOf(Parsedown::class, $this->app->make(Parsedown::class));
-
-        // Assert that the alias 'parsedown' is bound to the correct class
-        $this->assertInstanceOf(Parsedown::class, $this->app->make('parsedown'));
-
-        // Assert that the configuration has been merged
-        $this->assertTrue($this->app['config']->has('markdown'));
+        return [
+            ParsedownServiceProvider::class,
+        ];
     }
 }
